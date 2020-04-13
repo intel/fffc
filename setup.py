18a3add3843c880de894169a1185b8ff4442791d
William Roberts <william.c.roberts@intel.com>
2020-04-13 11:21:28 -0500

    setup: add pathlib dependency
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
     scripts=["scripts/fffc_log_inspector", "scripts/fffc_dedup_crashes", "scripts/fffc_crashtool", "scripts/fffc_internal_crashtool"],
     entry_points={"console_scripts": ["fffc = fffc.generate:main"]},
     package_data={"fffc": ["templates/*"]},
-    install_requires=["pyelftools", "pycparser"],
+    install_requires=["pyelftools", "pycparser", "pathlib" ],
     zip_safe=False,
 )
